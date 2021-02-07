8a9fe52e239989659297beac6e3527f4f5811fc5
Todd Carney <toddcarney44@gmail.com>
2021-02-06 18:16:34 -0700

    Add dtb interface to handlers; build step functions to carry out remote data processing

--- handler.py
+++ handler.py
@@ -1,4 +1,7 @@
+import os
 import json
+import boto3
+import arrow
 import getOdds
 import getResults
 from dtbInterface import DbInterface
@@ -8,65 +11,63 @@ def updateOdds(event, context):
     db = DbInterface()
     status, statusMsg, matches = getOdds.parseJson()
     db.updateOdds(matches)
-    
-    body = {
-        "message": statusMsg,
-        "matches": len(matches),
-        "input": event
-    }
+
+    body = {"message": statusMsg, "matches": len(matches), "input": event}
     statusCode = 400
     if status:
         statusCode = 200
-    
-    response = {
-        "statusCode": statusCode,
-        "body": json.dumps(body)
-    }
+
+    response = {"statusCode": statusCode, "body": json.dumps(body)}
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
 
 def updateResults(event, context):
+    db = DbInterface()
     status, statusMsg, results = getResults.parseJson()
+    updated = db.updateResults(results)
 
-    body = {
-        "message": statusMsg,
-        "results": len(results),
-        "input": event
-    }
+    body = {"message": statusMsg, "results": len(results), "input": event, 'updated': updated}
     statusCode = 400
     if status:
         statusCode = 200
-    
-    response = {
-        "statusCode": statusCode,
-        "body": json.dumps(body)
-    }
+
+    response = {"statusCode": statusCode, "body": json.dumps(body)}
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
+def ScheduleOdds(event, context):
+    waitInterval = 4
+    waitTime = getWaitTime(waitInterval)
+    
+    print(os.environ['ODDS_ARN'])
+    client = boto3.client('stepfunctions')
+    inputStr = '{"waitUntil": "' + waitTime +'"}'
+    response = client.start_execution(stateMachineArn=os.environ['ODDS_ARN'], input = inputStr)
+    
     return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
+        "message": 'I just scheduled the next odds update',
+        "executionArn": response['executionArn']
     }
-    """
 
-if __name__ == '__main__':
-    print('Testing updateOdds...')
-    resp = updateOdds('', '')
-    print(resp)
+def ScheduleResults(event, context):
+    waitInterval = 1
+    waitTime = getWaitTime(waitInterval)
 
-    print('Testing updateResults...')
-    resp = updateResults('', '')
-    print(resp)
+    client = boto3.client('stepfunctions')
+    inputStr = '{"waitUntil": "' + waitTime +'"}'
+    response = client.start_execution(stateMachineArn=os.environ['RESULTS_ARN'], input = inputStr)
+    
+    return {
+        "message": 'I just scheduled the next odds update',
+        "executionArn": response['executionArn']
+    }
+
+def getWaitTime(interval):
+    now = arrow.utcnow()
+    
+    time = now.shift(hours=interval)
+        
+    ret = time.for_json()
+    
+    return ret
